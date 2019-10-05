3362593d3c938bfbd5b4cde58ec535d5b7ca9d42
Cliff Wells <cliff.wells@gmail.com>
2019-10-04 17:20:21 -0700

    let the user decide how to handle exceptions

--- sysdmanager/systemd_manager.py
+++ sysdmanager/systemd_manager.py
@@ -45,185 +45,80 @@ class SystemdManager(object):
         self.__bus = dbus.SystemBus()
 
     def list_units(self):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            units = interface.ListUnits(byte_arrays=True)
-            return units
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
+        return self._interface().ListUnits()
 
     def list_jobs(self):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            return interface.ListJobs()
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
+        return self._interface().ListJobs()
 
     def start_unit(self, unit_name, mode="replace"):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            interface.StartUnit(unit_name, mode)
-            return True
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
+        self._interface().StartUnit(unit_name, mode)
+        return True
 
     def stop_unit(self, unit_name, mode="replace"):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            interface.StopUnit(unit_name, mode)
-            return True
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
+        self._interface().StopUnit(unit_name, mode)
+        return True
 
     def restart_unit(self, unit_name, mode="replace"):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            interface.RestartUnit(unit_name, mode)
-            return True
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
+        self._interface().RestartUnit(unit_name, mode)
+        return True
 
     def enable_unit(self, unit_name):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            interface.EnableUnitFiles([unit_name],
-                                      dbus.Boolean(False),
-                                      dbus.Boolean(True))
-            return True
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
+        self._interface().EnableUnitFiles(
+            [unit_name],
+            dbus.Boolean(False),
+            dbus.Boolean(True)
+        )
+        return True
 
     def disable_unit(self, unit_name):
-        interface = self._get_interface()
-
-        if interface is None:
-            return False
-
-        try:
-            interface.DisableUnitFiles([unit_name], dbus.Boolean(False))
-            return True
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
-
-    def _get_unit_file_state(self, unit_name):
-        interface = self._get_interface()
-
-        if interface is None:
-            return None
-
-        try:
-            state = interface.GetUnitFileState(unit_name)
-            return state
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return False
-
-    def _get_interface(self):
-        try:
-            obj = self.__bus.get_object("org.freedesktop.systemd1",
-                                        "/org/freedesktop/systemd1")
-            return dbus.Interface(obj, "org.freedesktop.systemd1.Manager")
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return None
+        self._interface().DisableUnitFiles([unit_name], dbus.Boolean(False))
+        return True
 
     def get_active_state(self, unit_name):
         properties = self._get_unit_properties(unit_name, self.UNIT_INTERFACE)
-
         if properties is None:
             return False
-
-        try:
-            state = properties["ActiveState"].encode("utf-8")
-            return state
-        except KeyError:
-            return False
+        return properties["ActiveState"].encode("utf-8") if "ActiveState" in properties else False
 
     def is_active(self, unit_name):
-        unit_state = self.get_active_state(unit_name)
-        return unit_state == b"active"
+        return self.get_active_state(unit_name) == b"active"
 
     def is_failed(self, unit_name):
-        unit_state = self.get_active_state(unit_name)
-        return unit_state == b"failed"
+        return self.get_active_state(unit_name) == b"failed"
 
     def get_error_code(self, unit_name):
         service_properties = self._get_unit_properties(unit_name, self.SERVICE_UNIT_INTERFACE)
-
         if service_properties is None:
             return None
-
         return self._get_exec_status(service_properties)
 
     def _get_exec_status(self, properties):
-        try:
-            exec_status = int(properties["ExecMainStatus"])
-            return exec_status
-        except KeyError:
-            return None
+        return int(properties["ExecMainStatus"]) if "ExecMainStatus" in properties else None
 
     def _get_result(self, properties):
-        try:
-            result = properties["Result"].encode("utf-8")
-            return result
-        except KeyError:
-            return False
+        return properties["Result"].encode("utf-8") if "Result" in properties else None
 
     def _get_unit_properties(self, unit_name, unit_interface):
-        interface = self._get_interface()
+        unit_path = self._interface().LoadUnit(unit_name)
+        obj = self.__bus.get_object("org.freedesktop.systemd1", unit_path)
+        properties_interface = dbus.Interface(obj, "org.freedesktop.DBus.Properties")
+        return properties_interface.GetAll(unit_interface)
 
-        if interface is None:
-            return None
+    def _get_unit_file_state(self, unit_name):
+        return self._interface().GetUnitFileState(unit_name)
 
-        try:
-            unit_path = interface.LoadUnit(unit_name)
+    def _interface(self):
+        obj = self.__bus.get_object(
+            "org.freedesktop.systemd1",
+            "/org/freedesktop/systemd1"
+        )
+        return dbus.Interface(obj, "org.freedesktop.systemd1.Manager")
 
-            obj = self.__bus.get_object(
-                "org.freedesktop.systemd1", unit_path)
-
-            properties_interface = dbus.Interface(
-                obj, "org.freedesktop.DBus.Properties")
-
-            return properties_interface.GetAll(unit_interface)
-
-        except dbus.exceptions.DBusException as error:
-            print(error)
-            return None
 
 
 if __name__ == "__main__":
     s = SystemdManager()
-    # print(s.get_error_code("wpa_supplicant.service"))
-    print(s.list_jobs())
-    print(s.list_units())
+    try:
+        print(s.list_units())
+    except dbus.exceptions.DBusException as error:
+        print(error)
